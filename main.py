03bbd49feaceb9859218aae683618b623de10915
Nusret <53660826+nusretcakir@users.noreply.github.com>
2023-03-26 21:35:37 +0300

    v2
    v2

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def yeni_dosya_indir():
     r = requests.get(url)
     open('main2.py', 'wb').write(r.content)
 
+    url = "https://raw.githubusercontent.com/nusretcakir/version_control/master/v.txt"
+    r = requests.get(url)
+    open('v.txt', 'wb').write(r.content)
+
 
 def version_test():
 
@@ -20,7 +24,7 @@ def version_test():
     new_version = int(str((BeautifulSoup(requests.get(url).content,"html.parser"))))
     
     if version != new_version:
-        print("program guncel değil") 
+        print("program v2 oldu") 
         yeni_dosya_indir()
         exit()
 
@@ -29,8 +33,8 @@ def ana():
     version_test()
 
     while True:
-        print("program v2 oldu")
-        print("test")
+        print("v1 v1")
+        print("deneme")
         time.sleep(2)
 
 ana()
